6c385a1f659ca423bc9f93d2cd60d4650be0a568
Colin Shield & Nitin Verma <pair+colin+nitin@modcloth.com>
2011-03-04 09:49:46 -0800

    sending stdout to /dev/null

--- sunspot/lib/sunspot/server.rb
+++ sunspot/lib/sunspot/server.rb
@@ -42,7 +42,7 @@ module Sunspot
         pid = fork do
           Process.setsid
           STDIN.reopen('/dev/null')
-#          STDOUT.reopen('/dev/null', 'a')
+          STDOUT.reopen('/dev/null', 'a')
           STDERR.reopen(STDOUT)
           run
         end
@@ -68,7 +68,7 @@ module Sunspot
         pid = fork do
           Process.setsid
           STDIN.reopen('/dev/null')
-#          STDOUT.reopen('/dev/null', 'a')
+          STDOUT.reopen('/dev/null', 'a')
           STDERR.reopen(STDOUT)
           master_run
         end
