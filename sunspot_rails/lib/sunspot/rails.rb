e817c3fba6d2833bda4a11256dcbdcc1a0e8ee24
Michael Fields <mfields1@gmail.com>
2014-08-21 15:01:43 -0400

    importing more rails 4 compatible code from latest sunspot/sunspot-rails pt 2
     (barf)

--- sunspot_rails/lib/sunspot/rails.rb
+++ sunspot_rails/lib/sunspot/rails.rb
@@ -6,9 +6,14 @@ require File.join(File.dirname(__FILE__), 'rails', 'searchable')
 
 module Sunspot #:nodoc:
   module Rails #:nodoc:
+    autoload :SolrInstrumentation, File.join(File.dirname(__FILE__), 'rails', 'solr_instrumentation')
     autoload :StubSessionProxy, File.join(File.dirname(__FILE__), 'rails', 'stub_session_proxy')
-    autoload :Server, File.join(File.dirname(__FILE__), 'rails', 'server')
-    autoload :VERSION, File.join(File.dirname(__FILE__), 'rails', 'version')
+    begin
+      require 'sunspot_solr'
+      autoload :Server, File.join(File.dirname(__FILE__), 'rails', 'server')
+    rescue LoadError
+      # We're fine
+    end
 
     class <<self
       attr_writer :configuration
@@ -22,10 +27,12 @@ module Sunspot #:nodoc:
       end
 
       def build_session(configuration = self.configuration)
-        if configuration.has_master?
+        if configuration.disabled?
+          StubSessionProxy.new(Sunspot.session)
+        elsif configuration.has_master?
           SessionProxy::MasterSlaveSessionProxy.new(
-            SessionProxy::ThreadLocalSessionProxy.new(master_config(configuration)),
-            SessionProxy::ThreadLocalSessionProxy.new(slave_config(configuration))
+              SessionProxy::ThreadLocalSessionProxy.new(master_config(configuration)),
+              SessionProxy::ThreadLocalSessionProxy.new(slave_config(configuration))
           )
         else
           SessionProxy::ThreadLocalSessionProxy.new(slave_config(configuration))
@@ -36,21 +43,29 @@ module Sunspot #:nodoc:
 
       def master_config(sunspot_rails_configuration)
         config = Sunspot::Configuration.build
-        config.solr.url = URI::HTTP.build(
-          :host => sunspot_rails_configuration.master_hostname,
-          :port => sunspot_rails_configuration.master_port,
-          :path => sunspot_rails_configuration.master_path
+        builder = sunspot_rails_configuration.scheme == 'http' ? URI::HTTP : URI::HTTPS
+        config.solr.url = builder.build(
+            :host => sunspot_rails_configuration.master_hostname,
+            :port => sunspot_rails_configuration.master_port,
+            :path => sunspot_rails_configuration.master_path,
+            :userinfo => sunspot_rails_configuration.userinfo
         ).to_s
+        config.solr.read_timeout = sunspot_rails_configuration.read_timeout
+        config.solr.open_timeout = sunspot_rails_configuration.open_timeout
         config
       end
 
       def slave_config(sunspot_rails_configuration)
         config = Sunspot::Configuration.build
-        config.solr.url = URI::HTTP.build(
-          :host => configuration.hostname,
-          :port => configuration.port,
-          :path => configuration.path
+        builder = sunspot_rails_configuration.scheme == 'http' ? URI::HTTP : URI::HTTPS
+        config.solr.url = builder.build(
+            :host => sunspot_rails_configuration.hostname,
+            :port => sunspot_rails_configuration.port,
+            :path => sunspot_rails_configuration.path,
+            :userinfo => sunspot_rails_configuration.userinfo
         ).to_s
+        config.solr.read_timeout = sunspot_rails_configuration.read_timeout
+        config.solr.open_timeout = sunspot_rails_configuration.open_timeout
         config
       end
     end
