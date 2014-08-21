6e050011cbe259628ec6659d239f6c2a6442091d
Michael Fields <mfields1@gmail.com>
2014-08-21 14:55:46 -0400

    importing more rails 4 compatible code from latest sunspot/sunspot-rails
     (barf)

--- sunspot_rails/lib/sunspot_rails.rb
+++ sunspot_rails/lib/sunspot_rails.rb
@@ -1,7 +1,11 @@
-require 'sunspot/rails'
-
-if Rails::VERSION::MAJOR == 3
-  require 'sunspot/rails/railtie'
+# This needs to be loaded before sunspot/search/paginated_collection
+# or #to_json gets defined in Object breaking delegation to Array via
+# method_missing
+if Rails::VERSION::MAJOR > 4 || (Rails::VERSION::MAJOR == 4 && Rails::VERSION::MINOR >= 1)
+  require 'active_support/core_ext/object/json'
 else
-  require 'sunspot/rails/init'
+  require 'active_support/core_ext/object/to_json'
 end
+
+require 'sunspot/rails'
+require 'sunspot/rails/railtie'
